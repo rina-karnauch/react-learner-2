9a655e4373eda1e112a948150e03036da33344ef
rina-karnauch <rina.k.karnauch@gmail.com>
2022-08-31 15:57:50 +0300

    added a headerCartButton

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logoImg from '../../assets/meals.jpeg';
 import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
     return (
         <React.Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton/>
             </header>
             <div className={classes['main-image']}>
                 <img src={logoImg} alt="food table with various meals"/>

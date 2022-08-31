e4c8617b7e644ad07f78b26cba96098f189e8d3a
rina-karnauch <rina.k.karnauch@gmail.com>
2022-08-31 15:51:42 +0300

    added a functional component of an header

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as logoImg from '../../assets/meals.jpeg';
+import logoImg from '../../assets/meals.jpeg';
 import classes from './Header.module.css';
 
 const Header = (props) => {
@@ -9,7 +9,7 @@ const Header = (props) => {
                 <h1>ReactMeals</h1>
                 <button>Cart</button>
             </header>
-            <div>
+            <div className={classes['main-image']}>
                 <img src={logoImg} alt="food table with various meals"/>
             </div>
         </React.Fragment>

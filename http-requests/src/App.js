481b778361044e34ea1fe260213ebce3ef31784f
rina-karnauch <rina.k.karnauch@gmail.com>
2022-09-01 16:31:26 +0300

    added loading while fetching

--- http-requests/src/App.js
+++ http-requests/src/App.js
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    function fetchMoviesHandler() {
+        setIsLoading(true);
+        fetch('https://swapi.dev/api/films/').then((response) => {
+            return response.json();
+        }).then((data) => {
+            const parsedMovies = data.results.map((movie) => {
+                return {
+                    id: movie.episode_id,
+                    title: movie.title,
+                    openingText: movie.opening_crawl,
+                    releaseDate: movie.release_date,
+                }
+            });
+            setMovies(parsedMovies);
+            setIsLoading(false);
+        });
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>
+                {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+                {!isLoading && movies.length === 0 && <p>no movies found.</p>}
+                {isLoading && <p>Loading...</p>}
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;

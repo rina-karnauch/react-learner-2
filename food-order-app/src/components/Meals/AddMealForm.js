c18f0457ead9eb01f25f208578dea8810bfe13ba
rina-karnauch <rina.karnauch@mail.huji.ac.il>
2022-09-11 15:56:39 +0300

    added a styling for a form

--- food-order-app/src/components/Meals/AddMealForm.js
+++ food-order-app/src/components/Meals/AddMealForm.js
@@ -3,45 +3,84 @@ import classes from './AddMealsForm.module.css';
 
 const AddMealForm = (props) => {
 
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState(0.00);
+
+    const [errorObject, setErrorObject] = useState({
+        name: false,
+        description: false,
+        price: false
+    })
+
+    const handleNameInput = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleDescriptionInput = (event) => {
+        setDescription(event.target.value);
+    }
+
+    const handlePriceInput = (event) => {
+        setPrice(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        let nameFlag = false;
+        let descFlag = false;
+        let priceFlag = false;
+
+        if (name.trim() === '') {
+            nameFlag = true;
+        }
+        if (description.trim() === '') {
+            descFlag = true;
+        }
+        if (!price) {
+            priceFlag = true;
+        }
+
+        setErrorObject({
+            name: nameFlag,
+            description: descFlag,
+            price: priceFlag
+        });
+
+    }
 
     return (
         <React.Fragment>
-            <form onSubmit={() => {
-            }}>
-                <div className={classes['form-input']}>
-                    <div>
-                        <label htmlFor='name'>name: </label>
+            <form onSubmit={handleSubmit}>
+                <div className={classes['form-input'] && classes['container']}>
+                    <label className={classes['form-label']} htmlFor='name'>
                         <input
+                            className={errorObject.name ? classes['input-error'] : ''}
                             type='text'
                             id='name'
-                            onChange={() => {
-                            }}
-                            onBlur={() => {
-                            }}
+                            placeholder='text'
+                            onChange={handleNameInput}
                         />
-                    </div>
-                    <div>
-                        <label htmlFor='description'>description: </label>
+                    </label>
+                    <label className={classes['form-label']} htmlFor='description'>
                         <input
+                            className={errorObject.description ? classes['input-error'] : ''}
                             type='text'
                             id='description'
-                            onChange={() => {
-                            }}
-                            onBlur={() => {
-                            }}
+                            placeholder='description'
+                            onChange={handleDescriptionInput}
                         />
-                    </div>
-                    <div>
-                        <label htmlFor='price'>price: </label>
+                    </label>
+                    <label className={classes['form-label']} htmlFor='price'>
                         <input
+                            className={errorObject.price ? classes['input-error'] : ''}
                             type='number'
                             id='price'
-                            onChange={() => {
-                            }}
-                            onBlur={() => {
-                            }}
+                            placeholder='price'
+                            onChange={handlePriceInput}
                         />
-                    </div>
+                    </label>
                 </div>
                 <button type="submit" className={classes['submit-button']}>submit</button>
             </form>
